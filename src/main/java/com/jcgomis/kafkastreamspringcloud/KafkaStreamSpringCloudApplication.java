ef381154d2462bacef1c18009997aef801443855
Jeangomistech <jeangomistech2018@gmail.com>
2026-01-28 10:19:53 +0100

    My first commit

---
+++ src/main/java/com/jcgomis/kafkastreamspringcloud/KafkaStreamSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.jcgomis.kafkastreamspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamSpringCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jcgomis/kafkastreamspringcloud/KafkaStreamSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcgomis.kafkastreamspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamSpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
